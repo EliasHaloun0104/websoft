cbb84d53e6cef7f8057456b0c4c6a489dac03cad
EliasHaloun0104 <elias.haloun0104@stud.hkr.se>
2020-02-06 12:15:53 +0100

    Re-add The deleted test on Node & Express

---
+++ work/S04/node/interpretator.js
@@ -0,0 +1,16 @@
+/**
+ * A sample of stating the famous Hello World.
+ * @author Elias Haloun
+ * @returns void
+ */
+
+console.log("Hello World");
+
+for (let i=0; i < 9; i++) {
+    console.log(i);
+}
+
+let a = 1;
+let b = a + 1;
+
+console.log(a, b);
