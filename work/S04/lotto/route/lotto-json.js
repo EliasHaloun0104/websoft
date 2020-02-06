ed37e92cf96cfbdd2c6572665425427404e92830
EliasHaloun0104 <elias.haloun0104@stud.hkr.se>
2020-02-06 14:35:23 +0100

    Rearrange the lotto code A04

--- work/S04/lotto/route/lotto-json.js
+++ work/S04/lotto/route/lotto-json.js
@@ -8,27 +8,23 @@ var router  = express.Router();
 var arr = [1,7,9,22,27,30,34];
 
 router.get("/", (req, res) => {
-    var clientNum = req.query.row;
-    var clientArr = clientNum.split(",");
-
-    console.log("arr " + parseInt(clientArr[0]));
-    var identical = 0;
-    for(var i= 0; i<arr.length; i++){
-        var num = parseInt(clientArr[i]);
-        if(arr.includes(num)){
-            identical++;
-        }
-    }
-    
     let data = {};     
     data.lottoNum = arr;
-    data.yourLotto = clientArr; 
-    data.identical = identical + " number/s";
-
-   
-
-
-    res.render("lotto-json", data);
+    //is there a parameter
+    if(req.query.row != null){
+        var clientNum = req.query.row;
+        var clientArr = clientNum.split(",");
+        var identical = 0;
+        for(var i= 0; i<arr.length; i++){
+            var num = parseInt(clientArr[i]);
+            if(arr.includes(num)){
+                identical++;
+            }
+        }
+        data.yourLotto = clientArr; 
+        data.identical = identical + " identical number/s";
+     }
+    res.send(JSON.stringify(data));
 });
 
 
--- work/S04/lotto/route/lotto.js
+++ work/S04/lotto/route/lotto.js
@@ -10,6 +10,25 @@ var arr = [1,7,9,22,27,30,34];
 router.get("/", (req, res) => {
     let data = {};     
     data.lottoNum = arr.toString(); 
+    
+    if(req.query.row != null){
+        var clientNum = req.query.row;
+        var clientArr = clientNum.split(",");
+        var identical = 0;
+        for(var i= 0; i<arr.length; i++){
+            var num = parseInt(clientArr[i]);
+            if(arr.includes(num)){
+                identical++;
+            }
+        }
+        data.yourLotto = clientArr; 
+        data.identical = identical + " identical number/s";
+        
+     }else{
+        data.yourLotto = ''; 
+        data.identical = ''; 
+     }
+
     res.render("lotto", data);
 });
 
